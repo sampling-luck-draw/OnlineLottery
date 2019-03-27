7883a990fa52b8aeb5e067c549655179bbd6b96c
w-mj <w.mingjian@yahoo.com>
2019-03-27 22:39:37 +0800

    push micro program register

--- MicroProgram/consumers.py
+++ MicroProgram/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import WebsocketConsumer
+from .models import Activity
 
 
 class CommandForward(WebsocketConsumer):
@@ -9,9 +10,15 @@ class CommandForward(WebsocketConsumer):
     def connect(self):
         self.accept()
         self.send("Who is that?")
+        Activity.objects.filter(id=4).update(channel_name=self.channel_name)
+        # print(self.channel_name)
 
     def receive(self, text_data=None, bytes_data=None):
         if text_data is None:
             return
         self.send("Oh " + text_data + " " + str(self.cnt))
         self.cnt += 1
+
+    def chat_message(self, event):
+        # Handles the "chat.message" event when it's sent to us.
+        self.send(text_data=event["text"])
