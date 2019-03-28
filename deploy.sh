d91f65afdf4ad948763f3d95d7b896bb05597f59
w-mj <w.mingjian@yahoo.com>
2019-03-28 19:32:58 +0800

    amazing background

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 git checkout -- .
 git pull origin master
-python3 manage.py collectstatic --noinput
-ssh -i ~/Login_tencent root@127.0.0.1 "supervisorctl restart sampling_service sampling_websocket"
+# python3 manage.py collectstatic --noinput
+ssh -i ~/Login_tencent root@127.0.0.1 "supervisorctl restart sampling_service sampling_websocket" &
